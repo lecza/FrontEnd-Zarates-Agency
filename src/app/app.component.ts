f0db485316e604b04e451a3abf54b57ab2adb5b9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-11-07 19:02:28 -0500

    [+] AppComponent: Ciclos de vida de un componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, OnDestroy, AfterContentInit, AfterViewChecked, AfterViewInit, AfterContentChecked, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-ecommerce';
+export class AppComponent implements OnInit, OnChanges, OnDestroy, AfterContentChecked, AfterContentInit, AfterViewInit, AfterViewChecked, DoCheck {
+
+  constructor() {
+    console.log( 'class AppComponent' );
+  }
+
+  ngAfterContentChecked(): void {
+    console.log( 'ngAfterContentChecked' )
+  }
+
+  ngAfterContentInit(): void {
+    console.log( 'ngAfterContentInit' );
+  }
+
+  ngAfterViewInit(): void {
+    console.log( 'ngAfterContentInit' );
+  }
+
+  ngAfterViewChecked(): void {
+    console.log( 'ngAfterViewChecked' );
+  }
+
+  ngDoCheck(): void {
+    console.log( 'ngDoCheck' );
+  }
+
+  ngOnDestroy(): void {
+    console.log( 'ngOnDestroy' );
+  }
+
+  ngOnInit(): void {
+    console.log( 'ngOnInit' );
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log( 'ngOnChanges' );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app.routing.module';
+
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
