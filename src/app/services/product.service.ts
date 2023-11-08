e5c51b97b115006c6e9d0c83f25516a38decb416
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-11-08 18:51:02 -0500

    [+] ng g environments [+] environment: Define baseURL [+] ProductService: Importa baseURL

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 import { ResponseProducts } from '../interfaces/response-products';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  // Atributos
+  BASE_URL: string = environment.baseUrl;
 
   constructor( private http: HttpClient ) { }
 
   getAllProducts() {
     
-    return this.http.get<ResponseProducts>( 'http://localhost:4001/api/products' );
+    return this.http.get<ResponseProducts>( `${ this.BASE_URL }/products` );
   }
 }
