6322398677c7123011561983f220b4af3e19987d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-11-07 21:18:55 -0500

    [+] ProductComponent: Obtiene datos usando FetchAPI (Vanilla JavaScript) & HttpClient (Angular) - EJEMPLO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app.routing.module';
 
@@ -13,7 +14,8 @@ import { ProductsComponent } from './pages/products/products.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Product } from 'src/app/interfaces/product';
 
 import { products } from 'src/app/fake-data/products';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-products',
@@ -12,7 +13,22 @@ export class ProductsComponent {
   // Atributos: Son la forma en que los datos van a estar disponibles para desplegarse en la View Componente
   products: Product[] = products;
 
-  // Constructor
+  // Constructor: public, private, proteted
+  constructor( private http: HttpClient ) {
+    this.getProducts();
+  }
+
+  async getProducts() {
+    // TypeScript + HttpClientModule (Angular)
+    this.http.get( 'http://localhost:4001/api/products' ).subscribe( ( data ) => {
+      console.log( data );
+    });
+
+    // JavaScript: FetchAPI
+    const response = await fetch( 'http://localhost:4001/api/products' );
+    const data = await response.json();
+    console.log( data );
+  }
 
   // Ciclos de vida
 
