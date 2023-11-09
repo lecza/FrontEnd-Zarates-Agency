d882304c2546ddeee53dd6a9a7d5998cf20752b6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-11-08 19:01:33 -0500

    [+] ng g c auth/pages/main, ng g c auth/pages/login, ng g c auth/pages/register

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { MainComponent } from './pages/main/main.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    MainComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
