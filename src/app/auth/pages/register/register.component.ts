8a907d5d6c9416b0d5544dcefd80d3e7cb10e953
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-11-09 12:16:28 -0500

    [+] auth.module: Importa ReactiveFormsModule [+] register.component: Define y vincula agrupacion de campos al formulario en la vista del componente

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  // Paso 2 (Reactive Forms): Define estructura
+  registerForm: FormGroup = this.formBuilder.group({
+    name: [ '' ],
+    username: [ '' ],
+    password: [ '' ]
+  });
 
+  // Paso 1 (Reactive Forms): Inyeccion de dependencias
+  constructor( private formBuilder: FormBuilder ) {}
+
+  // Paso 4 (Reactive Forms): Obtener datos del formulario
+  onSubmit() {
+    console.log( this.registerForm.value );
+  }
 }
