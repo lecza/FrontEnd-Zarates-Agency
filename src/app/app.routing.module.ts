fc3ddc3636a2faf2d39ed78b73bb225617d8bd2e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-11-08 19:29:19 -0500

    [+] app.routing.module: Define ruta para componente modular Auth [+] auth-routing.module: Define rutas de componentes del Modulo Auth

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /** Ruta a los componentes del Modulo Auth */
+  { 
+    path: 'auth',
+    loadChildren: () => import( './auth/auth.module' ).then( module => module.AuthModule )
+  },
+  /** Ruta para la redireccion de rutas desconocidas */
+  {
+    path: '**',
+    redirectTo: 'auth'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /** Rutas de los componentes del Modulo Auth */
+  {
+    path: '',                     // /auth
+    component: MainComponent,     // MainComponent
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'register',
+        component: RegisterComponent
+      },
+      /** Ruta para la redireccion de rutas desconocidas del Modulo Auth */
+      {
+        path: '**',
+        redirectTo: 'login'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
