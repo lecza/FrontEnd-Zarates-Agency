3340ce65b7e63d39bb2b527e3b75e2cb36f7dba7
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-11-09 22:01:32 -0500

    [+] login.components: Define HTML y Valida Formulario

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm: FormGroup = this.formBuilder.group({
+    email: [ '', [ Validators.required, Validators.email ] ],
+    password: [ '', [ Validators.required ] ]
+  });
 
+  constructor( private formBuilder: FormBuilder ) {}
+
+  login() {
+    console.log( this.loginForm.value );
+  }
 }
