804533bcf6a3d4963e916e8fe13d3ca15ee57eec
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-11-14 22:01:36 -0500

    [+] verify-auth: Permisiona la ruta en funcion a la existencia del token en el LocalStorage

--- src/app/guards/verify-auth.guard.ts
+++ src/app/guards/verify-auth.guard.ts
@@ -2,6 +2,8 @@ import { CanActivateFn } from '@angular/router';
 
 export const verifyAuthGuard: CanActivateFn = (route, state) => {
 
+
+
   /** El Guard siempre devuelve un valor Booleano */
   return ( localStorage.getItem( 'token' ) != null ) ? true : false;
 };
