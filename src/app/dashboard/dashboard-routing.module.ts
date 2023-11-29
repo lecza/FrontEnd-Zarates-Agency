f0b09d2587fbf0c322ec71c81a22f17aedbfe499
Luis Zarate <zaratecervantes1975@gmail.com>
2023-11-29 15:38:32 -0500

    Crea los componenes para el CRUD de Proyectos, define las rutas y agrega un menu de navegacion en el Dashboard

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -7,6 +7,9 @@ import { ProductsComponent } from './pages/products/products.component';
 import { verifyAuthGuard } from '../guards/verify-auth.guard';
 import { NewProductComponent } from './pages/products/new-product/new-product.component';
 import { UpdateProductComponent } from './pages/products/update-product/update-product.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { NewProjectComponent } from './pages/projects/new-project/new-project.component';
+import { UpdateProjectComponent } from './pages/projects/update-project/update-project.component';
 
 const routes: Routes = [
   {
@@ -16,7 +19,10 @@ const routes: Routes = [
       { path: '', component: DashboardComponent },
       { path: 'products', component: ProductsComponent },
       { path: 'products/new', component: NewProductComponent },
-      { path: 'products/update/:id', component: UpdateProductComponent }
+      { path: 'products/update/:id', component: UpdateProductComponent },
+      { path: 'projects', component: ProjectsComponent },
+      { path: 'projects/new', component: NewProjectComponent },
+      { path: 'projects/update', component: UpdateProjectComponent }
     ],
     canActivate: [ verifyAuthGuard ]
   }
