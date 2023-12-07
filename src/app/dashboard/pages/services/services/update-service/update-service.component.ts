6b55b5e5113f6a1b9e9e880648cd556d5181c6a1
Luis Zarate <zaratecervantes1975@gmail.com>
2023-12-07 14:22:31 -0500

    Obtener los datos de un servicio por id para obtener los datos y ponerlos en el formulario

--- src/app/dashboard/pages/services/services/update-service/update-service.component.ts
+++ src/app/dashboard/pages/services/services/update-service/update-service.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map, tap } from 'rxjs';
+import { Service } from 'src/app/interfaces/service';
+import { ServiceService } from 'src/app/services/service.service';
 import { ValidateFormsService } from 'src/app/services/validate-forms.service';
 
 @Component({
@@ -18,8 +21,37 @@ export class UpdateServiceComponent {
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
-    private validateForm: ValidateFormsService
+    private validateForm: ValidateFormsService,
+    private activatedRoute: ActivatedRoute,
+    private serviceServices: ServiceService
   ) {}
+
+  ngOnInit() {
+    this.activatedRoute.params
+      .pipe(
+        tap( response => {
+          console.log( response );  
+
+          return response;
+        }),
+        map( response => response[ 'id' ] )
+      ).subscribe( id  => {
+        console.log( id );      /// Extraemos de la URL
+
+        this.serviceServices.getServiceById( id ).subscribe( ( data: Service ) => {
+          console.log( data );
+
+          const { description, name, price } = data;
+
+          this.serviceForm.setValue({
+            description,
+            name,
+            price
+          });
+        })
+      });
+
+  }
 
   onSubmit() {
     console.log( this.serviceForm.value );
